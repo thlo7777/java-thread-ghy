455594bb7a2208c88fc94e404e892b47bb4c991d
thlo7777 <thlo7777@qq.com>
2021-03-12 19:27:48 +0800

    update

--- source/01 第一章/t7/src/run/Run.java
+++ source/01 第一章/t7/src/run/Run.java
@@ -3,10 +3,11 @@ package run;
 import mythread.MyThread;
 
 public class Run {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		MyThread mythread = new MyThread();
 		System.out.println("begin ==" + mythread.isAlive());
 		mythread.start();
+		//Thread.sleep(1000);
 		System.out.println("end ==" + mythread.isAlive());
 	}
 }
